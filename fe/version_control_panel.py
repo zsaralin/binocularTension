aa532acdacb1fe78dfeb3218d8f256811f6ed5aa
David Robert <david@antimodular.com>
2025-02-11 12:00:16 -0500

    Update window title in VersionControlPanel for clarity

--- fe/version_control_panel.py
+++ fe/version_control_panel.py
@@ -24,7 +24,7 @@ class VersionControlPanel(QWidget):
         self.version_selector = version_selector
         
         # Set window properties
-        self.setWindowTitle("User Controls - Binocular Tension 1.01")
+        self.setWindowTitle("Binocular Tension v1.00 - User GUI")
         
         # Install event filter to capture key events
         self.installEventFilter(self)
